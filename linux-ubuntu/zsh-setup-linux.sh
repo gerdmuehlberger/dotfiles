65060809762514d3f984fc71c7a837d12947cbca
zoidgerd <gerd.muehlberger96@gmail.com>
2025-05-24 01:38:48 +0200

    added initial vscode files

--- linux-ubuntu/zsh-setup-linux.sh
+++ linux-ubuntu/zsh-setup-linux.sh
@@ -16,7 +16,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
-#And add zsh-syntax-highlighting and zsh-syntax-highlighting to plugins and powerlevel10k theme in ~/.zshrc file
+#And add auto-suggestion and zsh-syntax-highlighting to plugins and powerlevel10k theme in ~/.zshrc file
 awk '
 /plugins=\(/ { gsub(/\)$/, " zsh-autosuggestions zsh-syntax-highlighting)"); print; next }
 /ZSH_THEME="robbyrussell"/ { print "ZSH_THEME=\"powerlevel10k/powerlevel10k\""; next }
