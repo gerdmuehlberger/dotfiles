648369581fc306d849c0e633116b319ce77319e4
zoidgerd <gerd.muehlberger96@gmail.com>
2025-05-24 00:18:19 +0200

    set up zsh

--- linux-ubuntu/zsh-setup-linux.sh
+++ linux-ubuntu/zsh-setup-linux.sh
@@ -11,3 +11,23 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # add syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Customise Zsh with Powerlevek10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+#And add zsh-syntax-highlighting and zsh-syntax-highlighting to plugins and powerlevel10k theme in ~/.zshrc file
+awk '
+/plugins=\(/ { gsub(/\)$/, " zsh-autosuggestions zsh-syntax-highlighting)"); print; next }
+/ZSH_THEME="robbyrussell"/ { print "ZSH_THEME=\"powerlevel10k/powerlevel10k\""; next }
+{ print }
+' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
+
+
+# Customise powerlevel10k theme
+# p10k configure
+
+
+# change default shell from bash to zsh
+chsh
+/bin/zsh
